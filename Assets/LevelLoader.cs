f10962a47e094559a132f824997b19354d0e9a51
JordiM17 <jordimulder88@gmail.com>
2021-01-07 22:12:24 +0100

    Script toegevoegd voor het switchen tussen scenes

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
+
+    private bool playerInZone;
+
+    public string levelToLoad;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        playerInZone = false;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.E) && playerInZone)
+        {
+            Application.LoadLevel(levelToLoad);
+        }
     }
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "BURLY-MAN_1_swordsman")
+        {
+            playerInZone = true;
+        }
+
+    }
+
+
 }
