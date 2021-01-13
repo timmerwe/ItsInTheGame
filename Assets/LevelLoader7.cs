5c20a1d4606179e4ad0aa7096a19b1101af87da8
JordiM17 <jordimulder88@gmail.com>
2021-01-13 20:06:17 +0100

    Alle mappen, colliders en portals af

--- Assets/LevelLoader7.cs
+++ Assets/LevelLoader7.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LevelLoader7 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    void OnTriggerEnter2D(Collider2D other)
+
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(7);
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
